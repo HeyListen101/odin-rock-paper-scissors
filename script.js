6749045b8ff53dbf96dc7d37164d5adab40b4694
jose <josematienzo101@gmail.com>
2023-08-17 09:31:52 +0800

    Add playRound and game function

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 const CHOICES = ['ROCK', 'PAPER', 'SCISSORS'];
-var playerScore = 0;
-var computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     return CHOICES[Math.floor(Math.random() * 3)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    let playerChoice = playerSelection.toUpperCase();
+    if (playerChoice == 'ROCK' && computerSelection == 'PAPER' ||
+        playerChoice == 'PAPER' && computerSelection == 'SCISSORS' ||
+        playerChoice == 'SCISSORS' && computerSelection == 'ROCK') {
+        computerScore++;
+        return 'You Lose';
+    } else if (playerChoice != computerSelection) {
+        playerScore++;
+        return 'You Win';
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let pChoice = prompt('Rock, Paper, or Scissors?');
+        console.log(playRound(pChoice, getComputerChoice()));
+    }
+    if (playerScore > computerScore) {
+        alert('You Win!');
+    } else if (playerScore < computerScore) {
+        alert('You Lose!');
+    } else {
+        alert("It's a Tie!");
+    }
 }
 
 console.log(getComputerChoice());
